faef6f4281fbf928a5741595eab5788bb2816282
Ola Flisbäck <ola.flisback@gmail.com>
2022-02-02 23:00:24 +0100

    Import my buffer next, prev mappings

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,1 +1,9 @@
 require "keymaps.which-key"
+
+local map = vim.api.nvim_set_keymap
+
+local opts = { noremap = true, silent = true }
+
+map('n', '<A-Left>', ':BufferPrevious<CR>', opts)
+map('n', '<A-Right>', ':BufferNext<CR>', opts)
+map('n', '<A-Down>', ':bd<CR>', opts)
