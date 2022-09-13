2a1aafe4e7b90a451a575e03925923fd86693954
Ola Flisbäck <x-ola.flisback@modelon.com>
2022-09-13 11:52:08 +0200

    Add plugins and eslint_d

--- lua/formatters/init.lua
+++ lua/formatters/init.lua
@@ -21,7 +21,7 @@ formatters.setup {
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   {
-    exe = "eslint",
+    exe = "eslint_d",
     filetypes = {
       "javascriptreact",
       "javascript",
