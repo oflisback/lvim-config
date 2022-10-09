2cd6742587ee48500a4a44f34a6efbbc537731ee
Ola Flisbäck <ola.flisback@gmail.com>
2022-10-09 16:44:28 +0200

    Add treesitter-context and leap

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -5,10 +5,10 @@ lvim.colorscheme = "onedarker"
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
-lvim.builtin.dashboard.active = true
+lvim.builtin.dashboard.active = false
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- lvim.builtin.nvimtree.show_icons.git = 0
 
 vim.opt.clipboard = 'unnamedplus'
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,10 @@
 require("plugins.barbar").config()
 require("plugins.treesitter").config()
+require('leap').set_default_keymaps()
 
 lvim.plugins = {
-  {"ruanyl/vim-gh-line"},
+  { "ruanyl/vim-gh-line" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "nvim-treesitter/nvim-treesitter-context" },
+  { "ggandor/leap.nvim" }
 }
